e8754b8feb9bf6ca8deec37fccc7402f54078ac6
Ben Attenborough <ben.attenborough@sfdsystems.com>
2019-05-06 20:59:08 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,32 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		<div className="board">
+			<div className="hex"></div>
+			<div className="hex"></div>
+			<div className="hex"></div>
+			<div className="hex"></div>
+			<div className="hex"></div>
+			<div className="hex"></div>
+			<div className="spacer"></div>
+			<div className="hex"></div>
+			<div className="hex"></div>
+			<div className="hex"></div>
+			<div className="hex"></div>
+			<div className="hex"></div>
+			<div className="hex"></div>
+			<div className="hex"></div>
+			<div className="hex"></div>
+			<div className="hex"></div>
+			<div className="hex"></div>
+			<div className="hex"></div>
+			<div className="spacer"></div>
+			<div className="hex"></div>
+			<div className="hex"></div>
+			<div className="hex"></div>
+			<div className="hex"></div>
+			<div className="hex"></div>
+		</div>
     </div>
   );
 }
